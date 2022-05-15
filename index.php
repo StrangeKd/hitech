ffa5756f7f640bdf43047ef2cab4f4857ae9e6d3
Ryan <102581367+StrangeKd@users.noreply.github.com>
2022-05-15 16:50:59 +0200

    push files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway&display=swap');
+    </style>
+    <meta charset="UTF-8">
+    <title>HiTech.net</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/normalize.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+
+<body>
+    <?php
+    require './view/header.phtml';
+    require './view/main.phtml';
+    require './view/footer.phtml';
+    ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!--<link rel="stylesheet" href="js/bootstrap.bundle.min.js" />-->
+    <!--<link rel="stylesheet" href="js/bootstrap.min.js" />-->
+</body>
+
+</html>
